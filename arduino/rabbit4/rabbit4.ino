4f67f47fcb185c49205006431254fa8226c69e37
gwentgod <ziyaozhu@connect.hku.hk>
2023-11-21 02:34:47 +0800

    dev: solar chaser control

--- arduino/rabbit4/rabbit4.ino
+++ arduino/rabbit4/rabbit4.ino
@@ -1,10 +1,13 @@
 void setup() {
+    base_setup();
+    chaser_setup();
+
     Serial.begin(115200);
     Serial.println("RABBIT4");
 
-    base_setup();
 }
 
 void loop() {
     base_loop();
+    chaser_loop();
 }
